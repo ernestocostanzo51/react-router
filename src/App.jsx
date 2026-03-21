83d008680e2954e02378b222c2e3fea098b06123
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-21 14:48:56 +0100

    aggiunta route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { BrowserRouter, Router, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-     
+     <BrowserRouter>
+     <Router>
+      <Route path='/' element={<homePage/>}></Route>
+      <Route path='/chisiamo' element={<chiSiamo/>}></Route>
+      <Route path='/prodotti' element={<Prodotti/>}></Route>
+     </Router>
+     </BrowserRouter>
     </>
   )
 }
